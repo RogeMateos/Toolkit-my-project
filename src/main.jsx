622a46935f3e3615c4b3d20fdc88939310f947dd
Roge <rogemateos@hotmail.com>
2024-03-07 21:56:43 +0000

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import reactDom from 'react-dom';
+import App from './App';
+import './styles/index.css';
+
+
+
+
+const container = document.getElementById('root');
+
+reactDom.render(<App />, container);
