af496736a95ba4ed12e8f0dfd2380cee26ca6f4d
Roge <rogemateos@hotmail.com>
2024-03-08 00:58:50 +0000

    fixing-configuring-slint-prettier
    fixing-configuring-slint-prettier

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import reactDom from 'react-dom';
-import App from './App';
-import './styles/index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-
-
-
-const container = document.getElementById('root');
-
-reactDom.render(<App />, container);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
