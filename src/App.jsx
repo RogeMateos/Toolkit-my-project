8e461680d02d4fcdcfd09a0bcf54f55cff2eaf96
Roge <rogemateos@hotmail.com>
2024-03-12 20:58:33 +0000

    Configuring-store-userlist-slicers

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import Navbar from "./components/Navbar";
-
+import UserList from "./components/UserList";
+// redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => (
+    <Provider store={store}>
     <Navbar />
+    <UserList />
+  </Provider>
 
 )
 export default App
